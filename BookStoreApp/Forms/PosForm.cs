3986070d0fdde20ee05238a2197225d4cd60cc14
Ddawng18 <nguyenhuynhdang000@gmail.com>
2026-06-09 17:54:24 +0700

    Add event handler for line discount type selection

--- BookStoreApp/Forms/PosForm.cs
+++ BookStoreApp/Forms/PosForm.cs
@@ -233,4 +233,12 @@ public partial class PosForm : Form
         DialogResult = DialogResult.Cancel;
         Close();
     }
+    
+    private void cboLineDiscountType_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (cboLineDiscountType.SelectedItem?.ToString() == DiscountType.None.ToString())
+        {
+            numLineDiscount.Value = 0;
+        }
+    }
 }
